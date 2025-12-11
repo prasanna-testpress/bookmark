a3549a4034c887fc4133302115d0dfd621d65016
prasanna-testpress <prasanna@testpress.in>
2025-12-11 11:52:18 +0530

    feat: Implement initial user account management including registration and profile editing.

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name='account'
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+      path('register/', views.register, name='register'),
+
+    # Edit profile
+    path('edit/', views.edit, name='edit'),
 
     # Authentication URLs (login, logout, password reset, etc)
     path('', include('django.contrib.auth.urls')),
--- bookmark/settings.py
+++ bookmark/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'account:dashboard'
 LOGIN_URL = 'account:login'
 LOGOUT_URL = 'account:logout'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [BASE_DIR / 'media']
