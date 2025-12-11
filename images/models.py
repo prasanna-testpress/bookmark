ebb75841ef97e8d08c5020bbaba44b012a9990df
prasanna-testpress <prasanna@testpress.in>
2025-12-11 12:45:03 +0530

    feat: Add Image model with fields, migration, and admin registration.

--- images/models.py
+++ images/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Image(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="images_created",
+        on_delete=models.CASCADE,
+    )
+
+    title = models.CharField(max_length=150)
+
+    slug = models.SlugField(max_length=200, blank=True)
+
+    url = models.URLField()
+
+    description = models.TextField()
+
+    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="images_liked", blank=True
+    )
+
+    def save(self):
+
+        if not self.slug:
+
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+
+        return self.title
