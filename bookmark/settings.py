c5c42e5e03722a56963b7af196783ef9eab8ffc4
prasanna-testpress <prasanna@testpress.in>
2025-12-11 11:14:12 +0530

    feat: Add basic user authentication with login, logout, and dashboard views.

--- bookmark/settings.py
+++ bookmark/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
+   
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'account:dashboard'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
