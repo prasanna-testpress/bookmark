952a36eb037afcd73b194fd76290c45eac5c51a5
prasanna-testpress <prasanna@testpress.in>
2025-12-11 11:31:25 +0530

    feat: Implement Django authentication system with password change and reset functionality, including templates, URLs, and settings.

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-from django.contrib.auth import views as auth_views
 
 app_name='account'
 urlpatterns = [
-    # path('login/', views.user_login, name='login'),
+    path('', views.dashboard, name='dashboard'),
 
-    path('login/', auth_views.LoginView.as_view(),name='login'),
-     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-     path('', views.dashboard, name='dashboard'),
-
+    # Authentication URLs (login, logout, password reset, etc)
+    path('', include('django.contrib.auth.urls')),
 ]
